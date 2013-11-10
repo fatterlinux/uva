968e97f371e1247f55a4f753590322d7875c63e5
loop <fatterlinux@gmail.com>
2013-11-10 11:07:00 +0800

    Update 10494.cpp

--- 10494.cpp
+++ 10494.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <string>
 
 using namespace std;
 void my_div(const string& a, long long b);
@@ -35,10 +36,17 @@ void my_mod(const string& a, long long b)
     cout << g << endl;
     return;
 }
+/*
+模拟手动除法
+e.g.:110/20  
+1）1/20不够，1*10 + 1 = 11
+2）11/20 不够，11*10 + 0= 110
+3）110/20够了，计算结果
+*/
 void my_div(const string& a, long long b)
 {
     long long g = 0;
-    int num[100000000];
+    int num[10000];
     long long  i = 0;
     memset(num, 0, sizeof(num));
     for (; i < a.size(); i++)
@@ -54,7 +62,8 @@ void my_div(const string& a, long long b)
     }
     if (valid_idx == i)
     {
-        cout <<0 << endl;
+        cout << 0 << endl;
+        return;//一个return导致多次wa啊
     }
     for (; valid_idx < i; valid_idx++)
     {
