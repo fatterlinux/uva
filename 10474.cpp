b464511586a24d4ff59b58f80e975d0260c95a95
loop <fatterlinux@gmail.com>
2013-11-11 20:50:01 +0800

    Update 10474.cpp

--- 10474.cpp
+++ 10474.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+int idx;
+int main()
+{
+    int a, b;
+    
+    while (cin >> a >> b)
+    {
+        if (!a && !b)
+        {
+            break;
+        }
+        idx++;
+        vector<int> v;
+        while (a--)
+        {
+            v.insert(a);
+        }
+        sort(v.begin, v.end);
+        cout << "CASE# " << idx << ":" << endl;
+        while (b--)
+        {
+            int t;
+            cin >> t;
+            int i;
+            for (i = 0; i < v.size(); i++)
+            {
+                if (v[i] == t)
+                {
+                    cout << t << " found at " << i << endl;
+                    break;
+                }
+            }
+            if (i == v.size())
+            {
+                cout << t << " not found" << endl;
+            }
+        }
+    }
+    return 0;
+}
