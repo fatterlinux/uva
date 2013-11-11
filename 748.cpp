71f7c3078396a760ecddd24259f030f537c08f97
loop <fatterlinux@gmail.com>
2013-11-11 08:50:30 +0800

    Update 748.cpp

--- 748.cpp
+++ 748.cpp
@@ -1,2 +1,59 @@
 #include <iostream>
-#include
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+cosnt int max_len = 3000;
+class bign
+{
+    char n[max_len];
+    int len;
+    
+    bign()
+    {
+        memset(n, 0, sizeof(n));
+        int len = 1;
+    }
+    ~bign(){}
+    
+    bign& operator=(const char* num)
+    {
+        len = strlen(num);
+        for (int i = 0; i < len; i++)
+        {
+            n[i] = num[len - 1 - i];
+        }
+        
+        return *this;
+    }
+    bign& operator=(int num)
+    {
+        char res[max_len];
+        sprintf(res, "%d", num);
+        *this = res;
+        
+        return *this;
+    }
+    bign(const char* num)
+    {
+        *this = num;
+    }
+    bign(int num)
+    {
+        *this = num;
+    }
+    string str() const
+    {
+        string res = "";
+        for (int i = 0; i < len; i++)
+        {
+            res = n[i] + res;
+        }
+        if (res.empty())
+        {
+            res = "0";
+        }
+        return res;
+    }
+};
