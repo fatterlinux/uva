dbcd05ee1d66e1a482b0a22098ac64b7ffdc5b95
loop <fatterlinux@gmail.com>
2013-11-12 18:06:39 +0800

    Update 120.cpp

--- 120.cpp
+++ 120.cpp
@@ -17,5 +17,42 @@ void filp(int arr[], int arr_len, int loc)
 
 int main()
 {
-  
+  string s;
+  while (getline(cin, s))
+  {
+    stringstream ss << s;
+    int a;
+    int arr[40];
+    int idx = 0;
+    while (ss >> a)
+    {
+      arr[idx++] = a;
+    }
+    bool first = true;
+    for (int i = idx - 1; i > 0; i--)
+    {
+      int max_idx = 0;
+      for (int j = 1; j <= i; j++)
+      {
+        if (arr[max_idx] < arr[j])
+        {
+          max_idx = j;
+        }
+      }
+      if (max_idx != i)
+      {
+        filp(arr, idx, max_idx);
+        filp(arr, idx, 1);
+        if (first) first = false;
+        else
+        {
+          cout << " ";
+        }
+        cout << max_idx << " " << 1;
+      }
+    }
+    if (!first) cout << " ";
+    cout << 0 <<endl;
+  }
+  return 0;
 }
