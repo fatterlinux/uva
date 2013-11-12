064ac0c70d83565898af86066d7386b66fe94373
loop <fatterlinux@gmail.com>
2013-11-12 17:45:17 +0800

    Update 120.cpp

--- 120.cpp
+++ 120.cpp
@@ -1,1 +1,21 @@
-#
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void filp(int arr[], int arr_len, int loc)
+{
+  int bottom = arr_len - loc;
+  for (int i = 0; i <= bottom/2; i++)
+  {
+    int t = arr[i];
+    arr[i] = arr[bottom - i];
+    arr[bottom - i] = t;
+  }
+  return;
+}
+
+int main()
+{
+  
+}
