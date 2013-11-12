8e30ecb312a691be8c810b585b42ed2f3be1580f
loop <fatterlinux@gmail.com>
2013-11-12 09:34:19 +0800

    Update 152.cpp

--- 152.cpp
+++ 152.cpp
@@ -1,1 +1,19 @@
-#
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+struct node
+{
+  double a, b, c;
+  node()
+  {
+    a = b = c = 0;
+  }
+};
+
+int distance_get(node& a, node& b)
+{
+  return int(sqrt(pow(a.a - b.a, 2) + pow(a.b - b.b, 2) + pow(a.c - b.c, 2)));
+}
