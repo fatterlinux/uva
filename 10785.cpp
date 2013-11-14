29a7375540db52cf5b86aa8ee699bcfc0ae7df89
loop <fatterlinux@gmail.com>
2013-11-14 16:47:56 +0800

    Update 10785.cpp

--- 10785.cpp
+++ 10785.cpp
@@ -1,1 +1,42 @@
-#
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  int test_case;
+  cin >>test_case;
+  while (test_case--)
+  {
+    int len;
+    cin >> len;
+    int idx = 1;
+    while (idx <= len)
+    {
+      if (idx % 2)//odd
+      {
+        
+      }
+      else //even
+      {
+        
+      }
+    }
+    sort(odd.begin(), odd.end());
+    sort(even.begin(), even.end());
+    for (int i = 1; i <= len; i++)
+    
+    {
+      if (i % 2)
+      {
+        cout << odd[i-1];
+      }
+      else
+      {
+        cout << even[i-1];
+      }
+    }
+    cout <<endl;
+  }
+}
