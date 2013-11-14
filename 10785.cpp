6c3cd0c02f95b03efedcda580674bb0e804fcebf
loop <fatterlinux@gmail.com>
2013-11-14 17:03:25 +0800

    Update 10785.cpp

--- 10785.cpp
+++ 10785.cpp
@@ -3,6 +3,43 @@
 
 using namespace std;
 
+struct node
+{
+  char m_c;
+  int m_times;
+  node()
+  {
+    m_c = 'A';
+    m_times = 0;
+  }
+  node& operator=(const node& i)
+  {
+    m_c = i.m_c;
+    m_times = i.times;
+    return *this;
+  }
+  void set_value(char c, int times)
+  {
+    m_c = c;
+    m_times = times;
+  }
+};
+struct odd_vowel
+{
+  node n[5];
+  odd_vowel()
+  {
+    n[0].set_value('A', 4);
+    n[1].set_value('U', 4);
+    n[2].set_value('E', 4);
+    n[3].set_value('O', 4);
+    n[4].set_value('I', 4);
+  }
+};
+char get_odd()//a, u, e, o, i
+{
+  for (int i = 0; i < )
+}
 int main()
 {
   int test_case;
@@ -12,15 +49,16 @@ int main()
     int len;
     cin >> len;
     int idx = 1;
+    string odd, even;
     while (idx <= len)
     {
       if (idx % 2)//odd
       {
-        
+        odd = odd + get_odd();
       }
       else //even
       {
-        
+        even = even + get_even();
       }
     }
     sort(odd.begin(), odd.end());
