6b98befaa799819813405e4bbdeb3c0db47f9d04
loop <fatterlinux@gmail.com>
2013-11-15 17:22:44 +0800

    Update 350.cpp

--- 350.cpp
+++ 350.cpp
@@ -1,1 +1,33 @@
-#
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+  int Z,I,M,L;
+  
+  while (cin >> Z >> I >> M >> L)//(z*l+i)%m
+  {
+    if (!Z && !I && !M && !L)
+    {
+      break;
+    }
+    int cycle[10000];
+    memset(cycle, 0, 10000);
+    int idx = 0;
+    int c = 0;
+    while (1)
+    {
+      cycle[idx] = (Z*L+i)%M;
+      L = cycle[idx];
+      for (int i = 0; i < idx; i++)
+      {
+        if (cycle[i] == cycle[idx])
+        {
+          c = idx - i;
+        }
+      }
+    }
+  }
+}
