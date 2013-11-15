ab110bea9b42e54c5d823e26da7cead66634507f
loop <fatterlinux@gmail.com>
2013-11-15 16:19:39 +0800

    Update 575.cpp

--- 575.cpp
+++ 575.cpp
@@ -1,1 +1,24 @@
-#
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  while (cin >> s)
+  {
+    if (s == "0")
+    {
+      break;
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+      sum += (s[i] - '0')*((1 << (s.size() - i)) - 1); 
+    }
+    cout << sum << endl;
+  }
+  return 0;
+}
