49f5243bd7fbabc3f19f70168e2c4e9eef426b45
loop <fatterlinux@gmail.com>
2013-11-19 09:16:10 +0800

    Update 11234.cpp

--- 11234.cpp
+++ 11234.cpp
@@ -1,1 +1,65 @@
-#
+#include <iostream>
+#include <stdlib.h>
+#include <queue>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+struct node
+{
+  char data;
+  node* l;
+  node* r;
+};
+
+node* node_create(char data, node* l, node* t)
+{
+  node* t  = (node*)malloc(sizeof(*t));
+  if (t)
+  {
+    t->data = data;
+    t->l = l;
+    t->r = r;
+  }
+  return t;
+}
+void node_free(node* t)
+{
+  if (!t) return;
+  node_free(t->l);
+  node_free(t->r);
+  free(t);
+  return ;
+}
+
+void bfs(node* t)
+{
+  if (!t) return;
+  queue<node*> q;
+  q.push(t);
+  string s;
+  while(!q.empty())
+  {
+    node* p = q.front();
+    q.pop();
+    
+    if (p->l) q.push(p->l);
+    if (p->r) q.push(p->r);
+    s = p->data + s;
+  }
+  cout << s;
+  return;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    string s;
+    cin >> s;
+    for ()
+  }
+}
