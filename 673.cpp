a5bc995bc8666fa023564833137692070390924b
loop <fatterlinux@gmail.com>
2013-11-24 12:42:54 +0800

    add 673

---
+++ 673.cpp
@@ -0,0 +1,52 @@
+#include <iostram>
+#include <stack>
+#include <string>
+
+uisng namespace std;
+
+int main(void)
+{
+    int test_case;
+    cin >> test_case;
+    while(test_case--)
+    {
+        string s;
+        cin >> s;
+        stack<char> ss;
+        bool correct_flag(true);
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '[')
+            {
+                ss.push(s[i]);
+            }
+            else if (s[i] == ')')
+            {
+                if (ss.top() != '(')
+                {
+                    correct_flag = false;
+                    break;
+                }
+                ss.pop();
+            }
+            else if (s[i] == ']')
+            {
+                if (ss.top() != '[')
+                {
+                    correct_flag = false;
+                    break;
+                }
+                ss.pop();
+            }
+        }//end for
+        if (ss.empty() && correct_flag)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
