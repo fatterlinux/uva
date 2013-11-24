56d3199028a94d3e5d655e87e1e0336bd2fa63eb
loop <fatterlinux@gmail.com>
2013-11-24 13:13:15 +0800

    Update 673.cpp

--- 673.cpp
+++ 673.cpp
@@ -1,4 +1,4 @@
-#include <iostram>
+#include <iostream>
 #include <stack>
 #include <string>
 #include <assert.h>
@@ -9,13 +9,14 @@ int main(void)
 {
     int test_case;
     cin >> test_case;
+    cin.get();//下面的getline会遇到整数后面的换行符，所以这里先丢弃换行符
     while(test_case--)
     {
         string s;
-        cin >> s;
+        getline(cin, s);
         stack<char> ss;
         bool correct_flag(true);
-        for (int i = 0; i < s.size(); i++)
+        for (size_t i = 0; i < s.size(); i++)
         {
             if (s[i] == '(' || s[i] == '[')
             {
@@ -23,7 +24,7 @@ int main(void)
             }
             else if (s[i] == ')')
             {
-                if (ss.top() != '(')
+                if (ss.empty() || ss.top() != '(')//可能输入不合法，empty需要check
                 {
                     correct_flag = false;
                     break;
@@ -32,7 +33,7 @@ int main(void)
             }
             else if (s[i] == ']')
             {
-                if (ss.top() != '[')
+                if (ss.empty() || ss.top() != '[')
                 {
                     correct_flag = false;
                     break;
@@ -44,7 +45,7 @@ int main(void)
                 assert(0);
             }
         }//end for
-        if (ss.empty() && correct_flag)
+        if (ss.empty() && correct_flag)//没有correct_flag的判断不行，假设就一个')'，ss是empty，可以flag不正确
         {
             cout << "Yes" << endl;
         }
