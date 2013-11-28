8ccb62d798d32c69023507c76d9e272dd03d9211
loop <fatterlinux@gmail.com>
2013-11-28 08:49:07 +0800

    Update 297.cpp

--- 297.cpp
+++ 297.cpp
@@ -1,1 +1,19 @@
-#
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int test_case;
+  cin >> test_case;
+  while (test_case--)
+  {
+    string s;
+    cin >> s;
+    int move;
+    counter cnt;
+    bfs(cnt, move = 0, 0, max_len - 1);
+    cin >> s;
+    bfs(cnt, move = 0, 0, max_len - 1);
+  }
+}
