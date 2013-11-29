695511365712ccf0823784503377859b0365d3bf
loop <fatterlinux@gmail.com>
2013-11-29 09:32:36 +0800

    Update 327.cpp

--- 327.cpp
+++ 327.cpp
@@ -19,7 +19,7 @@ int main()
     int map[26];
     for (int i = 0; i < 26; i++)
     {
-      map[i] = 0;
+      map[i] = i + 1;
     }
     sum_elm = var_get(var, s);
     int value = sum_get(s, sum_elm);
@@ -30,18 +30,31 @@ int main()
 
 vector<int> var_get(vector<char>& var, char* s)
 {
+  vector<int> v;
   for (int i = 0; i < strlen(s); i++)
   {
     if (islower(s[i]))
     {
+      var.push_back(s[i]);
       if (i >= 2 && is_pre_double_operator(s, i))
-      {}
+      {
+        map[s[i]-'a']++;
+        v.push_back(map[s[i] - 'a']);
+        s[i-1] = s[i-2] = ' ';
+      }
       else if ((i <= strlen(s) - 3) && is_post_double_operator(s, i))
-      {}
+      {
+        v.push_back(map[s[i] - 'a']);
+        map[s[i]-'a']++;
+        s[i+1] = s[i+2] = ' ';
+      }
       else
-      {}
+      {
+        v.push_back(map[s[i] - 'a']);
+      }
     }
   }
+  return v;
 }
 void filter_blank(char* s)
 {
