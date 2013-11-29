f01c7266c7a34b3d7ab69cac56a2b2db0f42fe3d
loop <fatterlinux@gmail.com>
2013-11-29 08:58:32 +0800

    Update 327.cpp

--- 327.cpp
+++ 327.cpp
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+const int max_len = 120;
+int main()
+{
+  char s[max_len];
+  while (gets(s))
+  {
+    filter_blank(s);
+    vector<int> var;
+    vector<int> sum_elm;
+    sum_elm = var_get(var, s);
+    int value = sum_get(s, sum_elm);
+    output(value, var);
+  }
+  return 0;
+}
