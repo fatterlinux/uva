e606a6dd107b868b204d8e224aac9dcd23deddb8
loop <fatterlinux@gmail.com>
2013-12-03 12:47:14 +0800

    Update 572.cpp

--- 572.cpp
+++ 572.cpp
@@ -2,12 +2,14 @@
 #include <string.h>
 
 int map[110][110];
+void dfs(int i, int j);
 int main()
 {
   int m, n;
   while (scanf(%d %d%*c, &m, &n) && m)
   {
     memset(map, 0, sizeof(map));
+    int cnt = 0;
     for (int i = 1; i <=m; i++)
     {
       char str[110];
@@ -25,8 +27,20 @@ int main()
     {
       for (int j = 1; j <= n; j++)
       {
-        if ()
+        if (map[i][j])
+        {
+          dfs(i, j);
+          cnt++;
+        }
       }
     }
   }
+  return 0;
+}
+
+const int dir[][2] = {()};
+void dfs(int i, int j)
+{
+  map[i][j] = 0;
+  if (map[])
 }
