6fecad10e2cd5bb67a3eba086b58d14caa3255a9
loop <fatterlinux@gmail.com>
2013-12-03 18:17:16 +0800

    Update 439.cpp

--- 439.cpp
+++ 439.cpp
@@ -12,6 +12,10 @@ struct node
   {
     return (x==a.x && y==b.y);
   }
+  bool check() const 
+  {
+    return (x >= 1 && x <= 8 && y >= 1 && y <=8);
+  }
 };
 void bfs(const node& a, const node& b);
 int main()
@@ -31,6 +35,39 @@ int main()
   }
   return 0;
 }
-static int dir
+void visit_set(const node& a)
+{
+  visit[a.x][a.y] = true;
+}
+bool visit_get(const node& a)
+{
+  return visit[a.x][a.y];
+}
+const int dir[][2] = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2},
+                        {-1, -2}, {-2, -1}};
 void bfs(const node& a, const node& b)
-{}
+{
+  if (a == b) return;
+  queue<node> q;
+  visit_set(a);
+  q.push(a);
+  
+  while (!q.empty())
+  {
+    node t = q.front();
+    q.pop();
+    for (int i = 0; i < 8; i++)
+    {
+      node next;
+      next.x = t.x + dir[i][0];
+      next.y = t.y + dir[i][1];
+      if (!next.check()) continue;
+      if (visit_get(next)) continue;
+      visit_set(next);
+      mark[next.x][next.y] = mark[t.x][t.y] + 1;
+      if (next == b) return;
+      q.push(next);
+    }
+  }
+  return;
+}
