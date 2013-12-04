ee7757ad0a4375dc39d8687e28c3570ba169c201
loop <fatterlinux@gmail.com>
2013-12-04 13:02:36 +0800

    Update 10004.cpp

--- 10004.cpp
+++ 10004.cpp
@@ -3,14 +3,67 @@
 #include <string.h>
 
 using namespace std;
-
-init()
-{}
+enum color_enum
+{
+  red = 0,
+  black,
+};
+const int max_len = 210;
+bool visit[max_len];
+bool color_visit[max_len];
+int edge[max_len][max_len];
+color_enm color[max_len];
+void init()
+{
+  memset(visit, 0, sizeof(visit));
+  memset(color_visit, 0, sizeof(color_visit));
+  memset(edge, 0, sizeof(edge));
+  memset(color, 0, sizeof(color));
+}
+void color_bfs(int idx, int n)
+{
+  queue<int> q;
+  color_visit[idx] = true;
+  color[idx] = red;
+  q.push(idx);
+  while (!q.empty())
+  {
+    int t = q.front();
+    q.pop();
+    for (int i = 0; i < max_len; i++)
+    {
+      if (edge[t][i] && !color_visit[i])
+      {
+        color_visit[i] = true;
+        color[i] = ((color[t] == red) ? black : red);
+        q.push(i);
+      }
+    }
+  }
+}
+bool dfs(int idx)
+{
+  visit[idx] = true;
+  for (int i = 0; i < max_len; i++)
+  {
+    if (!visit[i])
+    {
+      visit[i] = true;
+      if (color[i] == color[idx]) return false;
+      else
+      {
+        if (!dfs(i)) return false;
+      }
+    }
+  }
+  return true;
+}
 int main()
 {
   int n;
   while (cin >> n, n)
   {
+    init();
     int m;
     cin >> m;
     for (int i = 0; i < m; i++)
