cc7f6914d725c7faf16c073fdab71a466cd03921
loop <fatterlinux@gmail.com>
2013-12-05 14:15:53 +0800

    Update 784.cpp

--- 784.cpp
+++ 784.cpp
@@ -1,1 +1,64 @@
-#
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+void init();
+void input();
+void solve();
+void output();
+
+int main()
+{
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    init();
+    input();
+    solve();
+    output();
+  }
+  return 0;
+}
+const int max_row = 32;
+const int max_col = 82;
+char maze[max_row][max_col];
+bool visit[max_row][max_col];
+void init()
+{
+  memset(maze, 0, sizeof(maze));
+  memset(visit, 0, sizeof(visit));
+}
+void input(int& cnt)
+{
+  while (gets(maze[cnt]))
+  {
+    if (maze[cnt][0] == '_')
+    {
+      break;
+    }
+    cnt++;
+  }
+}
+void dfs(int m, int n, int cnt)
+{
+  visit[m][n] = true;
+  if (maze[m][n] == 'X') return;
+  if (maze[m][n] == ' ' || maze[m][n] == '*') maze[m][n] = '#';
+  
+}
+void solve(int cnt)
+{
+  for (int i = 0; i < cnt; i++)
+  {
+    for (int j = 0; j < strlen(maze[i]); j++)
+    {
+      if (maze[i][j] == '*' && visit[i][j])
+      {
+        dfs(i, j, cnt);
+      }
+    }
+  }
+}
