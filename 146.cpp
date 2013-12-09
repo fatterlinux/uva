acc4792e218db6e78ed262db048862161c3b0136
loop <fatterlinux@gmail.com>
2013-12-09 20:30:51 +0800

    Update 146.cpp

--- 146.cpp
+++ 146.cpp
@@ -1,1 +1,27 @@
-#
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+  char buf[55];
+  while (scanf("%s", buf))
+  {
+    if (buf[0] == '#')
+    {
+      break;
+    }
+    if (next_permutation(buf, buf+strlen(buf)))
+    {
+      printf("%s\n", buf);
+    }
+    else
+    {
+      printf("No Successor\n");
+    }
+  }
+  return 0;
+}
