d0fbd0e90bad86e862545fe5ab3c56746446a652
loop <fatterlinux@gmail.com>
2013-12-11 14:08:48 +0800

    Update 993.cpp

--- 993.cpp
+++ 993.cpp
@@ -6,5 +6,36 @@ int main()
   scanf("%d", &n);
   while (n--)
   {
+    int num;
+    scanf("%d", &num);
+    if (num == 1)
+    {
+      printf("1\n");
+      continue;
+    }
+    vector<int> v;
+    for (int i = 9; i > 1; i--)
+    {
+      while (num % i == 0)
+      {
+        v.push_back(i);
+        num /= i;
+      }
+      if (num == 1) break;
+    }
+    if (num != 1)
+    {
+      printf("-1\n");
+    }
+    else
+    {
+      sort(v.begin, v.end());
+      for (int i = 0; i < v.size(); i++)
+      {
+        printf("%d", v[i]);
+      }
+      printf("\n");
+    }
   }
+  return 0;
 }
