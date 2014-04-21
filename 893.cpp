7edd8e43edfdd00a767426269ba483a9bd7dc487
loop <fatterlinux@gmail.com>
2014-04-21 13:16:00 +0800

    Update 893.cpp

--- 893.cpp
+++ 893.cpp
@@ -9,23 +9,88 @@ class calc_date
   void date_show();
   private:
   int m_n, m_day, m_month, m_year;
-  static const int zero_day(1), zero_month(1), zero_year(1998);
   int days_from_zero_date(int day, int month, int year);
-  int date_from_zero_date(int n);
+  void date_from_zero_date(int n);
+  int days_for_year(y);
+  int days_for_month(m, y);
+  bool is_leap(int y);
 };
 
-static const int zero_day, zero_month, zero_year;
-
+bool calc_date::is_leap(int y)
+{
+  if (y % 4 && y % 100 != 0) return ture;
+  else if (y % 400 == 0) return ture;
+  else return false;
+}
+int calc_date::days_for_year(int y)
+{
+  return is_leap(y) ? 366 : 365;
+}
+int calc_date::days_for_month(int m, int y)
+{
+  static int m[2][13] = 
+  {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  };
+  return m[is_leap(y)][m];
+}
 void calc_date::date_show()
 {
   int days = days_from_zero_date(m_day, m_month, m_year);
-  days += m_n;
   date_from_zero_date(days);
 }
 
-int cacl_date::days_from_zero_date(int day, int month, int year)
+void cacl_date::date_show()
 {
-  int y(zero_year);
-  while (y)
-  
+  date_from_zero_date(days_from_zero_date());
+}
+int cacl_date::days_from_zero_date()
+{
+  int y(1998), m(1), d(1);
+  int days(m_n);
+  while (y < m_year)
+  {
+    days += days_for_year(y);
+    y++;
+  }
+  while (m < m_month)
+  {
+    days += days_for_month(m, y);
+    m++;
+  }
+  days += m_day - 1;
+  return days;
+}
+void calc_date::date_from_zero_date(int n)
+{
+  int y(1998), m(1), d(1);
+  while (n >= days_for_year(y))
+  {
+    n -= days_for_year(y);
+    y++;
+  }
+  while (n >= days_for_month(m))
+  {
+    n -= days_for_year(m);
+    m++;
+  }
+  d += n;
+  printf("%d %d %d\n", d, m, y);
+}
+
+int main()
+{
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  int n, d, m, y;
+  while (scanf("%d %d %d %d", &n, &d, &m, &y), !(n == 0&&d == 0&&m == 0&&y == 0))
+  {
+    
+    calc_date c(n, d, m, y);
+    c.date_show();
+  }
+  return 0;
 }
