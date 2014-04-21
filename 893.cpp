e2e0b30a2058a53f90f4c59e6c178a52fbafb8b1
loop <fatterlinux@gmail.com>
2014-04-21 09:02:40 +0800

    Update 893.cpp

--- 893.cpp
+++ 893.cpp
@@ -9,10 +9,23 @@ class calc_date
   void date_show();
   private:
   int m_n, m_day, m_month, m_year;
-  
+  static const int zero_day(1), zero_month(1), zero_year(1998);
+  int days_from_zero_date(int day, int month, int year);
+  int date_from_zero_date(int n);
 };
+
+static const int zero_day, zero_month, zero_year;
 
 void calc_date::date_show()
 {
+  int days = days_from_zero_date(m_day, m_month, m_year);
+  days += m_n;
+  date_from_zero_date(days);
+}
+
+int cacl_date::days_from_zero_date(int day, int month, int year)
+{
+  int y(zero_year);
+  while (y)
   
 }
