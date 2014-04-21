ba1f9bd78fe41235cdc49835e358e96e3fa5d8ce
loop <fatterlinux@gmail.com>
2014-04-21 08:55:45 +0800

    Update 893.cpp

--- 893.cpp
+++ 893.cpp
@@ -1,1 +1,18 @@
-#
+#include <stdio.h>
+
+using namespace std;
+
+class calc_date
+{
+  public:
+  calc_date(int n, int day, int month, int year) : m_n(n), m_day(day), m_month(month), m_year(year) {}
+  void date_show();
+  private:
+  int m_n, m_day, m_month, m_year;
+  
+};
+
+void calc_date::date_show()
+{
+  
+}
