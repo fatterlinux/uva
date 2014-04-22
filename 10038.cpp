8d53fa6c5704204de15dbfc184e50a805b2f3f9a
loop <fatterlinux@gmail.com>
2014-04-22 14:31:56 +0800

    Update 10038.cpp

--- 10038.cpp
+++ 10038.cpp
@@ -1,1 +1,42 @@
 #include <stdio.h>
+#include <math.h>
+
+#define max_n 3001
+bool map[max_n];
+long long a[max_n];
+int main()
+{
+  int n;
+  while (scanf("%d", &n) != EOF)
+  {
+    memset(map, 0, sizeof(map));
+    
+    for (int i = 0; i < n; i ++)
+    {
+      scanf("%d", &a[i]);
+    }
+    for (int i = 1; i < n; i++)
+    {
+      long long t = abs(a[i-1], a[i]);
+      if (t < (long long)n) map[t] = true;
+    }
+    bool flag = true;
+    for (int i = 1; i < n; i++)
+    {
+      if (!map[i])
+      {
+        flag = false;
+        break;
+      }
+    }
+    if (flag)
+    {
+      printf("Jolly\n");
+    }
+    else
+    {
+      printf("Not jolly");
+    }
+  }
+  return 0;
+}
