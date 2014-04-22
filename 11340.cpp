fe35adba09b6644291666e3d95fc45d8699a4568
loop <fatterlinux@gmail.com>
2014-04-22 20:02:34 +0800

    Update 11340.cpp

--- 11340.cpp
+++ 11340.cpp
@@ -1,1 +1,43 @@
-#
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+static int ascii[256];
+
+int main()
+{
+    int tets_case;
+    cin >> test_case;
+    while (test_case--)
+    {
+      memset(ascii, 0, sizeof(ascii));
+      int v_tbl;
+      cin >> v_tbl;
+      while (v_tbl--)
+      {
+        string s;
+        getline(cin, s);
+        char c;
+        int v;
+        cin >> c >> v;
+        ascii[c] = v;
+      }
+      int lines;
+      cin >> lines;
+      string s;
+      getline(cin, s);
+      double sum(0);
+      while(lines--)
+      {
+        getline(cin, s);
+        int size = s.size();
+        for (int i = 0; i < size; i++)
+        {
+          sum += ascii[s.at(i)];
+        }
+      }
+      printf("%.3lf$\n", sum / 100);
+    }
+    return 0;
+}
