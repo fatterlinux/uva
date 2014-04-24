054d474eca6ecb95f6354d174e49914af2417425
loop <fatterlinux@gmail.com>
2014-04-24 23:25:50 +0800

    Update 10901.cpp

--- 10901.cpp
+++ 10901.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <assert.h>
 
 using namespace std;
 
+static int run(queue<int>& q, queue<int>& other, int& time, int t, int n);
 int main()
 {
   int test_case;
@@ -28,33 +30,57 @@ int main()
       }
     }
     int time = 0;
+    int flag = -1;
     while (!(L.empty() && R.empty()))
     {
-      int max_size = n;
-      while (!L.empty() && L.front() <= time && max_size > 0)
-      {
-        cout << time + t << endl;
-        L.pop();
-        max_size--;
-      }
-      if (max_size == n && R.front() > time)
-      {
-        time = R.front();
-      }
-      time += t;
-      max_size = n;
-      while (!R.empty() && R.front() <= time && max_size > 0)
-      {
-        cout << time + t << endl;
-        R.pop();
-        max_size--;
-      }
-      if (max_size == n && L.front > time)
-      {
-        time = L.front();
-      }
-      time += t;
+        if (flag == -1)
+        {
+          flag = run(L, R, time, t, n);
+        }
+        else
+        {
+          flag = run(R, L, time, t, n);
+        }
     }
   }
   return 0;
+}
+
+static int run(queue<int> q, queue<int> other, int& time, int t, int n)
+{
+  int max_size = n;
+  while (!.empty() && q.front() <= t && max_size > 0)
+  {
+    cout << time + t << endl;
+    q.pop();
+    max_size--;
+  }
+  if (max_size != n)
+  {
+    time += t;
+    return 1;
+  }
+  else //说明本岸边没有车
+  {
+    if (other.front() <= t)
+    {
+      time += t;
+      return 1;
+    }
+    else if (q.front() <= other.front())
+    {
+      time = q.front();
+      return -1;
+    }
+    else if (q.front() > other.front())
+    {
+      time = t + other.front();
+      return 1;
+    }
+    else
+    {
+      assert(0);
+    }
+    return 0;
+  }
 }
