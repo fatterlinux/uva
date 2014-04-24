b984bb49becb4240122449ae596a1457ef402050
loop <fatterlinux@gmail.com>
2014-04-24 18:31:07 +0800

    Update 10901.cpp

--- 10901.cpp
+++ 10901.cpp
@@ -1,1 +1,60 @@
-#include 
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  int test_case;
+  cin >> test_case;
+  while (test_case--)
+  {
+    int n, t, m;
+    cin >> n >> t >> m;
+    queue<int> L, R;
+    while (m--)
+    {
+      string s;
+      int time;
+      cin >> time >> s;
+      if (s == "left")
+      {
+        L.push(time);
+      }
+      else 
+      {
+        R.push(time);
+      }
+    }
+    int time = 0;
+    while (!(L.empty() && R.empty()))
+    {
+      int max_size = n;
+      while (!L.empty() && L.front() <= time && max_size > 0)
+      {
+        cout << time + t << endl;
+        L.pop();
+        max_size--;
+      }
+      if (max_size == n)
+      {
+        time = R.front();
+      }
+      time += t;
+      max_size = n;
+      while (!R.empty() && R.front() <= time && max_size > 0)
+      {
+        cout << time + t << endl;
+        R.pop();
+        max_size--;
+      }
+      if (max_size == n)
+      {
+        time = L.front();
+      }
+      time += t;
+    }
+  }
+  return 0;
+}
