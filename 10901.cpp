9442ace5ac420521983c3c3bb4d8354d176350c7
loop <fatterlinux@gmail.com>
2014-04-26 00:45:14 +0800

    Update 10901.cpp

--- 10901.cpp
+++ 10901.cpp
@@ -3,10 +3,13 @@
 #include <string>
 #include <assert.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <utility>
 using namespace std;
 
-static int run(queue<int>& q, queue<int>& other, int& time, int t, int n);
+int timer_table[10000];
+typedef pair <int, int> elm;
+static int run(queue<elm>& q, queue<elm>& other, int& time, int t, int n);
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -15,24 +18,37 @@ int main()
 #endif
     int test_case;
     cin >> test_case;
+    bool p_flag = false;
     while (test_case--)
     {
+        if (p_flag)
+        {
+            cout << endl;
+        }
+        else
+        {
+            p_flag = true;
+        }
         int n, t, m;
+        memset(timer_table, 0, sizeof(timer_table));
         cin >> n >> t >> m;
-        queue<int> L, R;
-        while (m--)
+        queue<elm> L, R;
+        int h = 0;
+        while (h < m)
         {
             string s;
             int time;
             cin >> time >> s;
+            elm hh = make_pair(time, h);
             if (s == "left")
             {
-                L.push(time);
+                L.push(hh);
             }
             else 
             {
-                R.push(time);
+                R.push(hh);
             }
+            h++;
         }
         int time = 0;
         int bank = -1;
@@ -56,17 +72,21 @@ int main()
                 bank = bank * (-1);
             }
         }
-        cout << endl;
+        for (int i = 0; i < m; i++)
+        {
+            cout << timer_table[i] << endl;
+        }
     }
     return 0;
 }
 
-static int run(queue<int>& q, queue<int>& other, int& time, int t, int n)
+static int run(queue<elm>& q, queue<elm>& other, int& time, int t, int n)
 {
     int max_size = n;
-    while (!q.empty() && q.front() <= time && max_size > 0)
+    while (!q.empty() && q.front().first <= time && max_size > 0)
     {
-        cout << time + t << endl;
+        //cout << time + t << endl;
+        timer_table[q.front().second] = time + t;
         q.pop();
         max_size--;
     }
@@ -75,31 +95,31 @@ static int run(queue<int>& q, queue<int>& other, int& time, int t, int n)
         time += t;
         return 1;
     }
-    if (!other.empty() && other.front() <= time)
+    if (!other.empty() && other.front().first <= time)
     {
         time += t;
         return 1;
     }
     if (!q.empty() && other.empty())
     {
-        time = q.front();
+        time = q.front().first;
         return -1;
     }
     if (q.empty() && !other.empty())
     {
-        time = t + other.front();
+        time = t + other.front().first;
         return 1;
     }
     if (!q.empty() && !other.empty())
     {
         if (q.front() <= other.front())
         {
-            time = q.front();
+            time = q.front().first;
             return -1;
         }
         else
         {
-            time = t + other.front();
+            time = t + other.front().first;
             return 1;
         }
     }
