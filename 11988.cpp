b4c021a3311993480de83061d20e4eacf55831b4
loop <fatterlinux@gmail.com>
2014-04-30 10:28:37 +0800

    Update 11988.cpp

--- 11988.cpp
+++ 11988.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  while (getline(cin, s))
+  {
+    stringstream ss;
+    ss << s;
+    char c;
+    list<char> l;
+    list<char>::iterator it = l.begin();
+    while (ss >> c)
+    {
+      if (c == '[') it = l.begin();
+      else if (c == ']') it = l.end();
+      else
+      {
+        l.insert(it, c);
+      }
+    }
+    for (it = l.begin(); it != l.end(); it++)
+    {
+      cout << *it;
+    }
+    cout << endl;
+  }
+  return 0;
+}
