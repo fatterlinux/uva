8be0d43b55ac754fa4b4e8c64d85cd74a5623f78
loop <fatterlinux@gmail.com>
2014-05-12 18:44:55 +0800

    Update 11572.cpp

--- 11572.cpp
+++ 11572.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <bitset>
+
+const int max_size = 1000000001;
+using namespace std;
+
+bitset<max_size> b;
+
+int main()
+{
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    b.reset();
+    int m;
+    cin >> m;
+    while (m--)
+    {
+      int snowflake;
+      cin >> snowflake;
+      b.set(snowflake);
+    }
+    cout << b.count << endl;
+  }
+  return 0;
+}
+
+
