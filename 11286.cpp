43a20844fe6c279031014f2125835149ae8eec9b
loop <fatterlinux@gmail.com>
2014-05-13 10:50:54 +0800

    Update 11286.cpp

--- 11286.cpp
+++ 11286.cpp
@@ -9,6 +9,24 @@ int main()
   int n;
   while (cin >> n, n)
   {
-    
+    map< <vector<int>, int > m;
+    int popularity = 0;
+    while (n--)
+    {
+      vector<int> v(5);
+      for (int i = 0; i < 5; i++)
+      {
+        cin >> v(i);
+      }
+      sort(v.begin(), v.end());
+      m[v]++;
+      popularity = max(popularity, m[v]);
+    }
+    int total = 0;
+    for (map< vector<int>, int >::iterator iter = m.begin(); iter != m.end(); iter++)
+    {
+      if (iter->second == popularity) total++;
+    }
+    cout << total << endl;
   }
 }
