590ff55399ca97714d812696d3d153ecc048c8e6
loop <fatterlinux@gmail.com>
2014-05-13 12:25:28 +0800

    Update 10266.cpp

--- 10266.cpp
+++ 10266.cpp
@@ -10,14 +10,16 @@ int main()
   int test_case;
   cin >> test_case;
   bool first(true);
+  string s;
+  getline(cin, s);
+  getline(cin, s);
   while (test_case--)
   {
     if (first) first = false;
     else cout <<endl;
-    string s;
     map<string, int> m;
     int cnt = 0;
-    while (cin >> s)
+    while (getline(cin, s))
     {
       if (s.[0] == '\n') break;
       m[s]++;
@@ -26,7 +28,7 @@ int main()
     for (map<string, int>::iterator iter = m.begin(); iter != m.end(); iter++)
     {
       cout << iter->first << " ";
-      printf("%.4lf\n", ((double)iter->second/cnt));
+      printf("%.4lf\n", ((double)iter->second/cnt * 100));
     }
   }
   return 0;
