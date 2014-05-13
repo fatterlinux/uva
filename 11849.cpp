4599c82e257d554a029273badf7d54401ab53b51
loop <fatterlinux@gmail.com>
2014-05-13 14:56:34 +0800

    Update 11849.cpp

--- 11849.cpp
+++ 11849.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
 #include <set>
+
+using namespace std;
+
+int main()
+{
+  int n, m;
+  while (cin >> n >> m, m || n)
+  {
+    set<int> s;
+    while (n--)
+    {
+    int t;
+    
+    cin >> t;
+    s.insert(t);
+    }
+    int cnt = 0;
+    while (m--)
+    {
+      int t ;
+      cin >>t;
+      if (s.find(t) != s.end())
+      {
+        cnt++;
+      }
+    }
+    cout << cnt << endl;
+  }
+  return 0;
+}
