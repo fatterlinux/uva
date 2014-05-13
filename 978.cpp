0913b2ecabd108515705218759f6da3f91852d17
loop <fatterlinux@gmail.com>
2014-05-13 19:32:17 +0800

    循环中多次判断qg.empty()超时，另外用map会超时，可能也和这个有关

--- 978.cpp
+++ 978.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 static int battle[100001];
@@ -17,24 +18,31 @@ int main()
     else cout <<endl;
     int fields, green, blue;
     cin >> fields >> green >> blue;
-    multiset<int, greater<int> > sg, sl;
+    //multiset<int, greater<int> > sg, sl;
+    priority_queue<int> qg, qb;
     while (green--)
     {
       int t;
       scanf("%d", &t);
-      sg.insert(t);
+      //sg.insert(t);
+      qg.push(t);
     }
     while (blue--)
     {
       int t;
       scanf("%d", &t);
-      sl.insert(t);
+      //sl.insert(t);
+      qb.push(t);
     }
     //memset(battle, 0, sizeof(battle));
-    while (!sg.empty() && !sl.empty())
+    //while (!sg.empty() && !sl.empty())
+    while (!qg.empty() && !qb.empty())
     {
-      for (int i = 0; i < fields; i++)
+      int tmp = min(qg.size(), qb.size());
+      tmp = min(tmp, fields);
+      for (int i = 0; i < tmp; i++)
       {
+        #if 0
         if (!sg.empty() && !sl.empty())
         {
           int a = *sg.begin();
@@ -43,41 +51,51 @@ int main()
           sg.erase(sg.begin());
           sl.erase(sl.begin());
         }
-        else
+        #endif
+
         {
-          break;
+          int a = qg.top();
+          int b = qb.top();
+          battle[i] = a - b;
+          qg.pop();
+          qb.pop();
         }
+
       }
-      for (int i = 0; i < fields; i++)
+      for (int i = 0; i < tmp; i++)
       {
+        #if 0
         if (battle[i] > 0) sg.insert(battle[i]);
         else if (battle[i] < 0) sl.insert(-battle[i]);
+        #endif
+        if (battle[i] >0) qg.push(battle[i]);
+        else if (battle[i] < 0) qb.push(-battle[i]);
       }
       
     }
 
-    if (sg.empty() && sl.empty())
+    if (qg.empty() && qb.empty())
     {
       cout << "green and blue died" <<endl;
     }
-    else if (!sg.empty())
+    else if (!qg.empty())
     {
-      assert(sl.empty());
+      assert(qb.empty());
       cout << "green wins" << endl;
-      for (set<int>::iterator iter = sg.begin();iter != sg.end(); iter++)
+      while (!qg.empty())
       {
-        //cout << *iter <<endl;
-        printf("%d\n", *iter);
+        cout << qg.top() <<endl;
+        qg.pop();
       }
     }
-    else if (!sl.empty())
+    else if (!qb.empty())
     {
-      assert(sg.empty());
+      assert(qg.empty());
       cout << "blue wins" <<endl;
-      for (set<int>::iterator iter = sl.begin();iter != sl.end(); iter++)
+      while (!qb.empty())
       {
-        //cout << *iter << endl;
-        printf("%d\n", *iter);
+        cout <<qb.top() <<endl;
+        qb.pop();
       }
     }
     else assert(0);
