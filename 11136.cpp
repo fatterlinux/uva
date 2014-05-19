b4cbbff513e9097b5599518a5a47355370d8ba8f
loop <fatterlinux@gmail.com>
2014-05-19 09:59:25 +0800

    Update 11136.cpp

--- 11136.cpp
+++ 11136.cpp
@@ -1,1 +1,31 @@
 #include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+  int test_case;
+  while (cin >> test_case, test_case)
+  {
+    priority_queue<int> max_heap;
+    priority_queue<int> min_heap;
+    long long cost = 0;
+    while (test_case--)
+    {
+      int t;
+      cin >> t;
+      while (t--)
+      {
+        int n;
+        cin >> n;
+        max_heap.push(n);
+        min_heap.push(n);
+      }
+      cost += max_heap.top() - min_heap.top();
+      max_heap.pop();
+      min_heap.pop();
+    }
+    cout << cost << endl;
+  }
+  return 0;
+}
