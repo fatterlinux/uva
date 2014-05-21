185601fa3d0c9b4a1c7c7db2c4f315e905cca80d
loop <fatterlinux@gmail.com>
2014-05-21 09:10:00 +0800

    Update 11991.cpp

--- 11991.cpp
+++ 11991.cpp
@@ -2,3 +2,40 @@
 #include <vector>
 
 using namespace std;
+
+
+static vector<int> a[1000001];
+int main()
+{
+  int n, m;
+  while (cin >> n && cin >> m)
+  {
+    for(int i = 0; i < 1000001;i++)
+    {
+      a[i].clear();
+    }
+    int i = 1;
+    while (i <= n)
+    {
+      int t;
+      cin >> t;
+      a[t].push_back(i);
+      i++;
+    }
+    while (m--)
+    {
+      int aa, b;
+      cin >> aa >> b;
+      if (a[aa].size() <b)
+      {
+        cout << 0;
+      }
+      else
+      {
+        cout << a[aa][b-1];
+      }
+      cout <<endl;
+    }
+  }
+  return 0;
+}
