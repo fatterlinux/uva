cc652fab0f0f96a028631a7f317f7911f90bcf16
loop <fatterlinux@gmail.com>
2014-05-21 15:17:41 +0800

    Update 793.cpp

--- 793.cpp
+++ 793.cpp
@@ -17,6 +17,35 @@ class union_find
   }
   int find(int p)
   {
-    
+    while (id[p] != p) p = id[p];
+    return p;
   }
+  bool connected(int p, int q)
+  {
+    return find(p) == find(q);
+  }
+  void union(int p, int q)
+  {
+    int parent_p = find(p);
+    int parent_q = find(q);
+    if (parent_p == parent_q) return;
+    if (set[parent_p] > set[parent_q])
+    {
+      id[parent_q] = parent_p;
+      set[parent_p] += set[parent_q];
+    }
+    else
+    {
+      id[parent_p] = parent_q;
+      set[parent_q] += set[parent_p];
+    }
+  }
+  ~union_find()
+  {
+    delete [] id;
+    delete [] set;
+  }
+  private:
+  int[] id;
+  int[] set;
 };
