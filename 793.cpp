1eb451e01399477e07cb4aed24599f3fc34c1227
loop <fatterlinux@gmail.com>
2014-05-21 15:08:18 +0800

    Update 793.cpp

--- 793.cpp
+++ 793.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+
+using namespace std;
+
+class union_find
+{
+  public:
+  union_find(int num)
+  {
+    id = new int[num+1];
+    set = new int[num+1];
+    for (int i = 0; i <= num; i++)
+    {
+      id[i] = i;
+      set[i] = 1;
+    }
+  }
+  int find(int p)
+  {
+    
+  }
+};
