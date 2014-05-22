81a51a8c4b3f66298ae3c9bab7e7743d9c9d1caa
loop <fatterlinux@gmail.com>
2014-05-22 09:59:05 +0800

    Update 11503.cpp

--- 11503.cpp
+++ 11503.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
+
+typedef pair<string, <int, string> > elm;
+class uf
+{
+  public:
+  int find(string s)
+  {
+    string t = s;
+    while (t != m[t].second) t = m[t].second;
+    return m[t].first; 
+  }
+  connected(string a, string b)
+  private:
+  map<elm> m;
+};
