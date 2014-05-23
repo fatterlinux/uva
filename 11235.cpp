95592f2624247b62fb170c3369b8dc2738d920ec
loop <fatterlinux@gmail.com>
2014-05-23 09:10:20 +0800

    Update 11235.cpp

--- 11235.cpp
+++ 11235.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+
+using namespace std;
+
+typedef struct seg_s{
+  int s;
+  int e;
+  }seg_t;
+const int maxn = 100005;
+static seg_t node[maxn];//根据a数组，抽象点集
+static int a[maxn];
+static int index;
+typedef struct tree_s
+{
+  int l;int r;int m;
+}tree_t;
+static int tree[maxn<<2];
+
+st::st(int i, int l, int r)
+{
+  tree[i].l = l;
+  tree[i].r = r;
+  if (l == r) 
+  {
+    tree[i].m = node[l].m;
+  }
+}
