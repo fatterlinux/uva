4e22475b79724014a35e4f2b1baf86eb5562b67f
loop <loop@mylover-huan>
2014-08-09 19:08:22 +0800

    记忆法dp

--- 624.cpp
+++ 624.cpp
@@ -5,35 +5,37 @@
 
 using namespace std;
 const int max_n = 21;
-unsigned a[max_n];
-unsigned dp(int n, int v)
+int track[max_n];
+int s[max_n][100000];
+
+int dp(int n, int v)
 {
-    if (n == 0) return 0;
-    unsigned res1 = 0;
-    if (v >= a[n]) res1 = a[n] + dp(n - 1, v - a[n]);
-    unsigned res2 = 0;
-    res2 = dp(n - 1, v);
-    return max(res1, res2);
+	if (s[n][v] >= 0) return s[n][v];
+	if (n == 0) return s[n][v] = 0;
+	if (track[n - 1] <= v)
+	{
+		return s[n][v] = max(dp(n - 1, v), (track[n-1] + dp(n - 1, v-track[n-1])));
+	}
+	else
+	{
+		return s[n][v] = dp(n - 1, v);
+	}
 }
-
-
 int main()
 {
-    freopen("in.txt", "r", stdin);
-    int v;
-    while (cin >> v)
-    {
-        int n;
-        cin >> n;
-        memset(a, 0, sizeof(a));
-        int i = 1;
-        while (i <= n)
-        {
-           cin >> a[i];
-           i++;
-        }
-        cout << "num:" << dp(n, v) << endl;
-    }
-    return 0;
+#ifndef _ONLINE_JUDGE_
+	freopen("624.in", "r", stdin);
+#endif
+	int value, number_tracks;
+	while (scanf("%d %d", &value, &number_tracks) != EOF)
+	{
+		memset(track, 0, sizeof(track));
+		memset(s, -1, sizeof(s));
+		for (int i = 0; i < number_tracks; i++)
+		{
+			scanf("%d", &track[i]);
+		}
+		cout << "sum:" << dp(number_tracks, value) << endl;
+	}
+	return 0;
 }
-
